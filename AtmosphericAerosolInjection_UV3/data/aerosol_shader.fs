40703f2a22e68f0fd7d442d8e6ece0b124677bc3
Mark SubbaRao <msubbarao@adlerplanetarium.org>
2018-04-18 10:29:08 -0500

    initial commit

---
+++ AtmosphericAerosolInjection_UV3/data/aerosol_shader.fs
@@ -0,0 +1,27 @@
+uniform float uv_fade;
+uniform float uv_alpha;
+uniform vec4 particleColor;
+
+in vec2 texcoord;
+
+out vec4 fragColor;
+in float boost;
+
+void main(void)
+{
+	
+	float rad = length(texcoord);
+	
+
+		//make a circle
+		if (rad > 1.){
+			//discard;
+		}
+
+
+	vec4 color = particleColor;
+	color.a *=  uv_fade * uv_alpha;
+
+	fragColor = color;
+		
+}
